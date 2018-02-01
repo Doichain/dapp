e9bd9bcedc45e07203beb7c71970ed80defbcc5d
Dustin Hoffmann <dustin.hoffmann@icloud.com>
2018-02-01 14:09:01 +0100

    Renamed the file for consistent naming

---
+++ imports/modules/server/namecoin/get_key-pair.js
@@ -0,0 +1,19 @@
+import { Meteor } from 'meteor/meteor';
+import { randomBytes } from 'crypto';
+import secp256k1 from 'secp256k1';
+
+const getKeyPair = () => {
+  try {
+    let privateKey
+    do {privateKey = randomBytes(32)} while(!secp256k1.privateKeyVerify(privateKey))
+    let publicKey = secp256k1.publicKeyCreate(privateKey);
+    return {
+      privateKey: privateKey.toString('hex').toUpperCase(),
+      publicKey: publicKey.toString('hex').toUpperCase()
+    }
+  } catch(exception) {
+    throw new Meteor.Error('namecoin.getKeyPair.exception', exception);
+  }
+};
+
+export default getKeyPair;
