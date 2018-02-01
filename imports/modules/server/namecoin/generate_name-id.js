c9f4544a92200fa010267a83b0ee4c6bc77082ad
Dustin Hoffmann <dustin.hoffmann@icloud.com>
2018-02-01 14:07:47 +0100

    Implemented name id generation

---
+++ imports/modules/server/namecoin/generate_name-id.js
@@ -0,0 +1,24 @@
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+import { OptIns } from '../../../api/opt-ins/opt-ins.js';
+import getKeyPair from './get_key-pair.js';
+
+const GenerateNameIdSchema = new SimpleSchema({
+  id: {
+    type: String
+  }
+});
+
+const generateNameId = (optIn) => {
+  try {
+    const ourOptIn = optIn;
+    GenerateNameIdSchema.validate(ourOptIn);
+    const nameId = getKeyPair().privateKey;
+    OptIns.update({_id : ourOptIn.id}, {$set:{nameId: nameId}});
+    return nameId;
+  } catch(exception) {
+    throw new Meteor.Error('namecoin.generateNameId.exception', exception);
+  }
+};
+
+export default generateNameId;
