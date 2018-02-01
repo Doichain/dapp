6db5154f0486aa24b9ab892b23ebfd70804ebd4a
Dustin Hoffmann <dustin.hoffmann@icloud.com>
2018-02-01 14:28:58 +0100

    Key pair is update-safe now

--- imports/modules/server/namecoin/get_key-pair.js
+++ imports/modules/server/namecoin/get_key-pair.js
@@ -4,9 +4,10 @@ import secp256k1 from 'secp256k1';
 
 const getKeyPair = () => {
   try {
-    let privateKey
-    do {privateKey = randomBytes(32)} while(!secp256k1.privateKeyVerify(privateKey))
-    let publicKey = secp256k1.publicKeyCreate(privateKey);
+    let privKey
+    do {privKey = randomBytes(32)} while(!secp256k1.privateKeyVerify(privKey))
+    const privateKey = privKey;
+    const publicKey = secp256k1.publicKeyCreate(privateKey);
     return {
       privateKey: privateKey.toString('hex').toUpperCase(),
       publicKey: publicKey.toString('hex').toUpperCase()
