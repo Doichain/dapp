1499b65f66f9fcc5a58f2b0772c1b6e3e4cb7a99
Dustin Hoffmann <dustin.hoffmann@icloud.com>
2018-02-08 15:40:45 +0100

    Removed tests

--- server/api/rest/rest.js
+++ server/api/rest/rest.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { SEND_APP, CONFIRM_APP, isAppType } from '../../../imports/startup/server/type-configuration.js';
-import checkNewTransactions from '../../../imports/modules/server/namecoin/check_new_transactions.js';
 
 export const Api = new Restivus({
   apiPath: 'api/',
@@ -11,16 +10,3 @@ export const Api = new Restivus({
 
 if(isAppType(SEND_APP)) import './send.js'
 if(isAppType(CONFIRM_APP)) import './confirm.js'
-
-Api.addRoute('test', {authRequired: false}, {
-  get: {
-    action: function() {
-      try {
-        checkNewTransactions();
-        return {status: 'success', data: {message: 'Test successfull'}};
-      } catch(error) {
-        return {statusCode: 500, body: {status: 'fail', message: error.message}};
-      }
-    }
-  }
-});
