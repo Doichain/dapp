6293ced10462913a632fe5a67d7071368d64feb8
Dustin Hoffmann <dustin.hoffmann@icloud.com>
2018-02-13 14:18:33 +0100

    Implemented getPrivateKey function

--- server/api/namecoin.js
+++ server/api/namecoin.js
@@ -4,8 +4,15 @@ import namecoin from 'namecoin';
 const NAMESPACE = 'e/';
 
 export function getPrivateKey(client, address) {
-  //TODO: Implement real privateKey rpc fetching
-  return "892B8AF44E535D646E7B01767579F5C4FBB9B9A09229AB4A15A8D14AB06977CA";
+  const syncFunc = Meteor.wrapAsync(namecoin_dumpprivkey);
+  return syncFunc(client, address);
+}
+
+function namecoin_dumpprivkey(client, address, callback) {
+  const ourAddress = address;
+  client.cmd('dumpprivkey', ourAddress, function(err, data) {
+    callback(err, data);
+  });
 }
 
 export function listSinceBlock(client, block) {
