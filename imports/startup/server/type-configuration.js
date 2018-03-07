ef8844522895cc62f5a4ca7a9a598609e3ce85bb
Dustin Hoffmann <dustin.hoffmann@icloud.com>
2018-03-07 13:34:30 +0100

    Added settings check

--- imports/startup/server/type-configuration.js
+++ imports/startup/server/type-configuration.js
@@ -3,6 +3,7 @@ export const SEND_APP = "send";
 export const CONFIRM_APP = "confirm";
 export const VERIFY_APP = "verify";
 export function isAppType(type) {
+  if(Meteor.settings === undefined || Meteor.settings.app === undefined) throw "No settings found!"
   const types = Meteor.settings.app.types;
   if(types !== undefined) return types.includes(type);
   return false;
