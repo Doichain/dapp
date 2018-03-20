3156f6d866145cea00d86bf27d989f3c9b544793
Ocean15 <dustin.hoffmann@icloud.com>
2018-03-20 14:00:22 +0100

    Fixed typo

--- imports/startup/server/dapp-configuration.js
+++ imports/startup/server/dapp-configuration.js
@@ -1,11 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+
 export function isDebug() {
   if(Meteor.settings !== undefined &&
      Meteor.settings.app !== undefined &&
      Meteor.settings.app.debug !== undefined) return Meteor.settings.app.debug
   return false;
 }
-export funciont getUrl() {
+
+export function getUrl() {
   if(Meteor.settings !== undefined &&
      Meteor.settings.app !== undefined &&
      Meteor.settings.app.host !== undefined) {
