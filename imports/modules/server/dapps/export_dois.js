5405ce2e8dc06fb9dc3e8f5957aef5c8a056bda8
Nico Krause <nico@le-space.de>
2018-06-23 18:58:54 +0500

    removed fetch

--- imports/modules/server/dapps/export_dois.js
+++ imports/modules/server/dapps/export_dois.js
@@ -31,7 +31,7 @@ const exportDois = (data) => {
 
     //if(ourData.status==1) query = {"confirmedAt": { $exists: true, $ne: null }}
 
-    const optIns = OptIns.aggregate(pipeline).fetch();
+    const optIns = OptIns.aggregate(pipeline);
 
     if(optIns === undefined) throw "Opt-In not found";
     logSend('Opt-Ins found',optIns);
