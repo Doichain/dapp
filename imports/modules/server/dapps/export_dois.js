bbb24775e901dda57dc403d22e9567fd5b76bdcb
Nico Krause <nico@le-space.de>
2018-06-23 20:35:06 +0500

    beautified and improved aggregate pipeline

--- imports/modules/server/dapps/export_dois.js
+++ imports/modules/server/dapps/export_dois.js
@@ -18,42 +18,21 @@ const exportDois = (data) => {
     const ourData = data;
     ExportDoisDataSchema.validate(ourData);
 
-    //var db = MongoInternals.defaultRemoteCollectionDriver().mongo.db;
-    //https://dev4devs.com/2015/06/05/meteor-js-how-to-use-aggregate-function/
-      //{ $match: {"confirmedAt":{ $exists: true, $ne: null }} },
     let pipeline = [
-        { $lookup: { from: "recipients", localField: "recipient", foreignField: "_id", as: "RecipientEmail" } }];
-
-    /*
+        { $match: {"confirmedAt":{ $exists: true, $ne: null }} },
+        { $lookup: { from: "recipients", localField: "recipient", foreignField: "_id", as: "RecipientEmail" } },
         { $lookup: { from: "senders", localField: "sender", foreignField: "_id", as: "SenderEmail" } },
         { $unwind: "$SenderEmail"},
         { $unwind: "$RecipientEmail"},
         { $project: {"_id":1,"createdAt:":1, "confirmedAt":1,"nameId":1, "SenderEmail.email":1,"RecipientEmail.email":1}}
-     */
-
+    ];
     //if(ourData.status==1) query = {"confirmedAt": { $exists: true, $ne: null }}
 
     let optIns =  OptIns.aggregate(pipeline);
-    console.log("Explain Report:", JSON.stringify(optIns));
-   /* db.collection("opt-ins").aggregate(pipeline,
-     Meteor.bindEnvironment(
-          function(err, result){
-              if(err) throw err;
-
-              _.each(result, function(str){console.log(str)});
-
-              optIns = result;
-          }
-      )); */
-    //logSend('Opt-Ins found',optIns);
-
-
     let exportDoiData;
     try {
-
         exportDoiData = optIns;
-      logSend('exportDoi url:',DOI_MAIL_FETCH_URL,JSON.stringify(exportDoiData));
-
+        logSend('exportDoi url:',DOI_MAIL_FETCH_URL,JSON.stringify(exportDoiData));
       return exportDoiData
 
     } catch(error) {
