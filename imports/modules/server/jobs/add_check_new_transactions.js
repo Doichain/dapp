f2065ec52422c131a25e26ed9252f1d25516e249
Nico Krause <nico@le-space.de>
2018-07-11 13:04:25 +0500

    changed job config

--- imports/modules/server/jobs/add_check_new_transactions.js
+++ imports/modules/server/jobs/add_check_new_transactions.js
@@ -4,7 +4,7 @@ import { BlockchainJobs } from '../../../../server/api/blockchain_jobs.js';
 const addCheckNewTransactionsBlockchainJob = () => {
   try {
     const job = new Job(BlockchainJobs, 'checkNewTransaction', {});
-    job.retry({retries: 60, wait: 60*1000 }).save({cancelRepeats: true});
+    job.retry({retries: 60, wait: 15*1000 }).save({cancelRepeats: true});
   } catch (exception) {
     throw new Meteor.Error('jobs.addCheckNewTransactionsBlockchain.exception', exception);
   }
