81540adb575343e376fd2436da9ab843f5e7d1b0
Nico Krause <nico@le-space.de>
2018-07-11 12:35:15 +0500

    changed name of job

--- imports/modules/server/jobs/add_check_new_transactions.js
+++ imports/modules/server/jobs/add_check_new_transactions.js
@@ -3,7 +3,7 @@ import { BlockchainJobs } from '../../../../server/api/blockchain_jobs.js';
 
 const addCheckNewTransactionsBlockchainJob = () => {
   try {
-    const job = new Job(BlockchainJobs, 'checkNewTransactions', {});
+    const job = new Job(BlockchainJobs, 'checkNewTransaction', {});
     job.retry({retries: 60, wait: 60*1000 }).repeat({repeats: Job.forever, wait: 1*60*1000}).save({cancelRepeats: true});
   } catch (exception) {
     throw new Meteor.Error('jobs.addCheckNewTransactionsBlockchain.exception', exception);
