65e16dc08246063c64d385014a1598b6d60b8a32
Nico Krause <nico@le-space.de>
2018-07-17 20:27:13 +0500

    fixed double ip as array

--- imports/modules/server/doichain/update.js
+++ imports/modules/server/doichain/update.js
@@ -28,7 +28,7 @@ const UpdateSchema = new SimpleSchema({
 const update = (data) => {
   try {
     const ourData = data;
-    console.log('----->'+JSON.stringify(ourData)+" isArray:"+ourData.host.constructor === Array);
+    console.log('----->'+JSON.stringify(ourData));
     if(ourData.host.constructor === Array) ourData.host = ourData.host[0];
     console.log('----->'+JSON.stringify(ourData)+" isArray:"+ourData.host.constructor === Array);
     UpdateSchema.validate(ourData);
