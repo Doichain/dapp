a152e61386e7d3ae652e2b4206ced49d7239dbf9
Nico Krause <nico@le-space.de>
2018-07-25 18:40:04 +0500

    fixed lint errors

--- server/api/rest/rest.js
+++ server/api/rest/rest.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import './imports/debug.js'
 import { isDebug } from '../../../imports/startup/server/dapp-configuration.js';
 import { SEND_APP, CONFIRM_APP, VERIFY_APP, isAppType } from '../../../imports/startup/server/type-configuration.js';
 
@@ -16,7 +17,9 @@ export const Api = new Restivus({
   prettyJson: true
 });
 
-if(isDebug()) import './imports/debug.js'
-if(isAppType(SEND_APP)) import './imports/send.js'
-if(isAppType(CONFIRM_APP)) import './imports/confirm.js'
-if(isAppType(VERIFY_APP)) import './imports/verify.js'
+//if(isAppType(SEND_APP))
+//if(isAppType(CONFIRM_APP))
+//if(isAppType(VERIFY_APP))
+import './imports/send.js'
+import './imports/confirm.js'
+import './imports/verify.js'
