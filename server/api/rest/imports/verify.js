9a1d359aa9894e10d53e1f824b5e4e1f0f92b78e
Nico Krause <nico@le-space.de>
2018-07-25 18:24:51 +0500

    changed eslint config

--- server/api/rest/imports/verify.js
+++ server/api/rest/imports/verify.js
@@ -3,12 +3,12 @@ import verifyOptIn from '../../../../imports/modules/server/opt-ins/verify.js';
 
 Api.addRoute('opt-in/verify', {authRequired: true}, {
   get: {
-    roleRequired: ['admin'],
+    roleRequired: [`admin`],
     action: function() {
       const params = this.queryParams;
       try {
         const val = verifyOptIn(params);
-        return {status: 'success', data: {val}};
+        return {status: "success", data: {val}};
       } catch(error) {
         return {statusCode: 500, body: {status: 'fail', message: error.message}};
       }
