e9aa5ac300c991a81406b0b0dddfe669986324c9
Nico Krause <nico@le-space.de>
2018-07-31 23:04:00 +0500

    added test to list transactions

--- server/basic-rest-api.app-test.0.js
+++ server/basic-rest-api.app-test.0.js
@@ -97,10 +97,9 @@ describe('basic-rest-api-app-test', function () {
 
 
     it('should have a balance bigger then 0 in the doichain wallet', function (done) {
-        //check funding
         //curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getbalance", "params": ["*", 6] }' -H 'content-type: text/plain;' http://127.0.0.1:18339
         const urlGetBalance = 'http://localhost:18443/';
-        const dataGetBalance = {"jsonrpc": "1.0", "id":"curltest", "method": "getbalance", "params": [] };
+        const dataGetBalance = {"jsonrpc": "1.0", "id":"getbalance", "method": "getbalance", "params": [] };
         const headersGetBalance = { 'Content-Type':'text/plain'  };
         const auth = "admin:generated-password";
         //curl -X POST -H 'X-User-Id: a7Rzs7KdNmGwj64Eq' -H 'X-Auth-Token: Y1z8vzJMo1qqLjr1pxZV8m0vKESSUxmRvbEBLAe8FV3' -i 'http://SEND_DAPP_HOST:3000/api/v1/opt-in?recipient_mail=<your-customer-email@example.com>&sender_mail=info@doichain.org'
@@ -109,8 +108,21 @@ describe('basic-rest-api-app-test', function () {
         //console.log(resultGetBalance.data.result);
         logBlockchain('resultGetBalance:',resultGetBalance);
         chai.assert.isAbove(resultGetBalance.data.result, 0, 'no funding! ');
-
         done();
-});
+    });
+
+    it('should list all transactions and check if our SOI is inside', function (done) {
+
+        const urlListTransactions = 'http://localhost:18443/';
+        const dataListTransactions = {"jsonrpc": "1.0", "id":"listtransactions", "method": "listtransactions", "params": [] };
+        const headersListTransaction = { 'Content-Type':'text/plain'  };
+        const auth = "admin:generated-password";
+        //curl -X POST -H 'X-User-Id: a7Rzs7KdNmGwj64Eq' -H 'X-Auth-Token: Y1z8vzJMo1qqLjr1pxZV8m0vKESSUxmRvbEBLAe8FV3' -i 'http://SEND_DAPP_HOST:3000/api/v1/opt-in?recipient_mail=<your-customer-email@example.com>&sender_mail=info@doichain.org'
+        const realdataListTransactions = { auth: auth, data: dataListTransactions, headers: headersListTransaction };
+        const resultListTransactions = getHttpPOST(urlListTransactions, realdataListTransactions);
+        logBlockchain('resultListTransactions:',resultListTransactions);
+        done();
+
+    });
 
 })
