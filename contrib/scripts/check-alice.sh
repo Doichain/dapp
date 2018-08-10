fde5cf1bbba22c30449293c5a11419274d99feca
Nico Krause <nico@le-space.de>
2018-08-10 15:50:54 +0500

    docker in pipeline and side container

--- contrib/scripts/check-alice.sh
+++ contrib/scripts/check-alice.sh
@@ -9,6 +9,6 @@ docker exec alice sudo netstat -atn
 docker exec alice doichain-cli -getinfo
 docker exec alice doichain-cli getnetworkinfo
 
-docker exec alice bash -c "curl -s --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://$MY_IP:18443/ | jq"
-echo curl -s --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://$MY_IP:18445/ | jq
+docker exec alicecurl -s --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://$MY_IP:18443/
+echo curl -s --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://$MY_IP:18445/
 echo curl -s --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://$MY_IP:18543/
