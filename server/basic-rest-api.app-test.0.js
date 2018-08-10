6d5a865eca846b7c2784161dfae75f08dfabc429
Nico Krause <nico@le-space.de>
2018-08-10 17:49:12 +0500

    connect alice & bob and create a regtest blockchain

--- server/basic-rest-api.app-test.0.js
+++ server/basic-rest-api.app-test.0.js
@@ -58,7 +58,7 @@ describe('basic-doi-test', function () {
         const resultGetPeerInfo = getHttpPOST(url, realdataGetPeerInfo);
         const statusGetPeerInfo = resultGetPeerInfo.statusCode;
         chai.assert.equal(200, statusGetPeerInfo);
-        chai.expect(resultGetPeerInfo.data.result).that.is.an("array").with.a.length(1);
+        chai.expect(resultGetPeerInfo.data.result).to.have.lengthOf(1);
        // chai.expect(resultGetPeerInfo.data.result).to.not.be.empty;
         logBlockchain('resultGetPeerInfo:',resultGetPeerInfo);
 
