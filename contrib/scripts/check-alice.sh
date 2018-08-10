c636aa5a4ee279a8adcda4e65f9ec4fa5086acf5
Nico Krause <nico@le-space.de>
2018-08-10 15:29:02 +0500

    docker in pipeline and side container

--- contrib/scripts/check-alice.sh
+++ contrib/scripts/check-alice.sh
@@ -3,5 +3,9 @@
 MY_IP=$(sudo docker inspect alice | jq '.[0].NetworkSettings.IPAddress' | tr -d \")
 echo $MY_IP
 docker ps
+docker exec alice doichain-cli -getinfo
+docker exec alice doichain-cli -getinfo
+docker exec alice doichain-cli getnetworkinfo
+
 #curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://$MY_IP:18443/
-curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://$MY_IP:18543/
+#curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://$MY_IP:18543/
