8e8c2f75e7ad88f7cfa1ed190db3e503e5ee18f7
Nico Krause <nico@le-space.de>
2018-08-10 11:47:44 +0500

    using curl to test alice

--- contrib/scripts/check-alice.sh
+++ contrib/scripts/check-alice.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-echo (sudo docker inspect alice | jq '.[0].NetworkSettings.IPAddress')
+#echo (sudo docker inspect alice | jq '.[0].NetworkSettings.IPAddress')
+curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:18443/
