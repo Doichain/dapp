09fc7ea3661242ec5fcb7908e93f5136b828e629
Nico Krause <nico@le-space.de>
2018-08-10 15:34:47 +0500

    docker in pipeline and side container

--- contrib/scripts/check-alice.sh
+++ contrib/scripts/check-alice.sh
@@ -3,9 +3,9 @@
 MY_IP=$(sudo docker inspect alice | jq '.[0].NetworkSettings.IPAddress' | tr -d \")
 echo $MY_IP
 docker ps
-docker exec alice apt-get update
-docker exec alice apt install net-tools
-docker exec alice netstat -atn
+docker exec alice sudo apt-get update
+docker exec alice sudo apt install net-tools
+docker exec alice sudo netstat -atn
 docker exec alice doichain-cli -getinfo
 docker exec alice doichain-cli getnetworkinfo
 
