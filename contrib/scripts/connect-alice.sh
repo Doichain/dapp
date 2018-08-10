04ac8ab5fbe7ae94304497f71e8b77fa8fb51ac6
Nico Krause <nico@le-space.de>
2018-08-10 17:30:16 +0500

    connect alice & bob and create a regtest blockchain

--- contrib/scripts/connect-alice.sh
+++ contrib/scripts/connect-alice.sh
@@ -2,5 +2,5 @@
 
 ALICE_IP=$(sudo docker inspect alice | jq '.[0].NetworkSettings.IPAddress' | tr -d \")
 #BOB_IP=$(sudo docker inspect bob | jq '.[0].NetworkSettings.IPAddress' | tr -d \")
-
+echo $ALICE_IP
 docker exec bob curl -s --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "addnode", "params": ["$ALICE_IP","onetry"] }' -H 'content-type: text/plain;' http://localhost:18443/
