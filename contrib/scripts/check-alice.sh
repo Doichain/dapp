4056d5ab9adf66c6fe0fd9f4ef7e747c5d47dbfe
Nico Krause <nico@le-space.de>
2018-08-10 11:51:47 +0500

    changed check-alice

--- contrib/scripts/check-alice.sh
+++ contrib/scripts/check-alice.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 #echo (sudo docker inspect alice | jq '.[0].NetworkSettings.IPAddress')
-curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://127.0.0.1:18443/
+curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://localhost:18443/
+curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://localhost:18332/
