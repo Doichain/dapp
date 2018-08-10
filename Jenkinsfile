d78f5853a028b08360532120da873daeb448e56f
Nico Krause <nico@le-space.de>
2018-08-10 15:11:33 +0500

    docker in pipeline and side container

--- Jenkinsfile
+++ Jenkinsfile
@@ -27,7 +27,7 @@
 node {
     checkout scm
 
-    docker.image("doichain/node-only").withRun("-it --name=alice -e REGTEST=true -e RPC_ALLOW_IP=::/0 -p 18543:18332 -e RPC_PASSWORD=generated--password -e RPC_HOST=localhost -e DAPP_HOST=alice -e DAPP_SMTP_HOST=localhost -e DAPP_SMTP_USER=doichain -e DAPP_SMTP_PASS='doichain-mail-pw!' -e DAPP_SMTP_PORT=25 -e CONFIRM_ADDRESS=xxx -e DEFAULT_FROM='reply@your-domain.com'") { c ->
+    docker.image("doichain/node-only").withRun("-it --name=alice -e REGTEST=true -e RPC_ALLOW_IP=::/0 -p 18543:18332 -e RPC_PASSWORD=generated-password -e RPC_HOST=localhost -e DAPP_HOST=alice -e DAPP_SMTP_HOST=localhost -e DAPP_SMTP_USER=doichain -e DAPP_SMTP_PASS='doichain-mail-pw!' -e DAPP_SMTP_PORT=25 -e CONFIRM_ADDRESS=xxx -e DEFAULT_FROM='reply@your-domain.com'") { c ->
                     sh 'docker logs alice'
                     sh 'sleep 10'
                     sh 'docker logs alice'
--- contrib/scripts/check-alice.sh
+++ contrib/scripts/check-alice.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-echo $(sudo docker inspect alice | jq '.[0].NetworkSettings.IPAddress')
+MY_IP=$(sudo docker inspect alice | jq '.[0].NetworkSettings.IPAddress')
+echo  $MY_IP
 docker ps
 sleep 10
-#curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://localhost:18443/
-curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://localhost:18543/
+#curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://$MY_IP:18443/
+curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://$MY_IP:18543/
