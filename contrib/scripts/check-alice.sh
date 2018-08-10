036865ff49518082cb4ba9cd52b5f62b3b34d282
Nico Krause <nico@le-space.de>
2018-08-10 15:25:01 +0500

    docker in pipeline and side container

--- contrib/scripts/check-alice.sh
+++ contrib/scripts/check-alice.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-MY_IP=$(sudo docker inspect alice | jq '.[0].NetworkSettings.IPAddress')
-echo  ${$MY_IP//"}
-MY_IP=${$MY_IP//"}
+MY_IP=$(sudo docker inspect alice | jq '.[0].NetworkSettings.IPAddress' | tr -d \")
+echo $MY_IP
 docker ps
 ping $MY_IP
 sleep 10
