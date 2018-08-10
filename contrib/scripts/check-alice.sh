dda2dd2b45ecbba5709dfdcf57eb557e4880e26b
Nico Krause <nico@le-space.de>
2018-08-10 11:57:37 +0500

    added sleep before check

--- contrib/scripts/check-alice.sh
+++ contrib/scripts/check-alice.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 #echo (sudo docker inspect alice | jq '.[0].NetworkSettings.IPAddress')
+docker ps
 sleep 10
-curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://localhost:18443/
+#curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://localhost:18443/
 curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://localhost:18332/
