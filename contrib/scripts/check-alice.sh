bac123964637ef291f61f15d888f6f5650a42d68
Nico Krause <nico@le-space.de>
2018-08-10 11:59:19 +0500

    changed port

--- contrib/scripts/check-alice.sh
+++ contrib/scripts/check-alice.sh
@@ -4,4 +4,4 @@
 docker ps
 sleep 10
 #curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://localhost:18443/
-curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://localhost:18332/
+curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "-getinfo", "params": [] }' -H 'content-type: text/plain;' http://localhost:18543/
