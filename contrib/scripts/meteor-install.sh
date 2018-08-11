d84bdbe389017c67db3a06032bcc85f0384e4b30
Nico Krause <nico@le-space.de>
2018-08-11 15:39:22 +0500

    added check if meteor is installed. if not installed meteor gets installed.

--- contrib/scripts/meteor-install.sh
+++ contrib/scripts/meteor-install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl https://install.meteor.com | /bin/sh
+command -v meteor >/dev/null 2>&1 || { echo >&2 "I require meteor but it's not installed.  Installing."; curl https://install.meteor.com | /bin/sh; }
+
