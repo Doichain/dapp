59990c0fc16622476a0a2e4ac9b3fadadbed66e1
Nico Krause <nico@le-space.de>
2018-08-13 11:33:44 +0500

    added echo

--- contrib/scripts/check-bob.sh
+++ contrib/scripts/check-bob.sh
@@ -9,8 +9,8 @@ docker exec bob sudo netstat -atn
 docker exec bob doichain-cli -getinfo
 docker exec bob doichain-cli getnetworkinfo
 
-echo "checking node from inside docker cointainer"
+echo "checking bob's node from inside docker cointainer"
 docker exec bob curl -s --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://$MY_IP:18443/
 
-echo "checking node from outside the docker cointainer (localhost)"
+echo "checking bob's node from outside the docker cointainer (localhost)"
 curl --user admin:generated-password --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://localhost:18544/
