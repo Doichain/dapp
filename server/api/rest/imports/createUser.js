d5783070238743120e07cfc49ee40539103211fc
Florian Holzer <floarian.holzer@webanizer.com>
2018-10-02 16:46:57 +0200

    added custom post function with input validation

--- server/api/rest/imports/createUser.js
+++ server/api/rest/imports/createUser.js
@@ -1,7 +1,29 @@
 import { Api } from '../rest.js';
 import {Meteor} from 'meteor/meteor';
+import {Accounts} from 'meteor/accounts-base'
+import SimpleSchema from 'simpl-schema';
 
-
+const createUserSchema = new SimpleSchema({
+    username: {
+      type: String,
+      regEx: "^[A-Z,a-z,0-9,!,_,$,#]{4,24}$"
+    },
+    email: {
+      type: String,
+      regEx: SimpleSchema.RegEx.Email
+    },
+    password: {
+      type: String,
+      regEx: "^[A-Z,a-z,0-9,!,_,$,#]{8,24}$"
+    },
+    profile: {
+        type: SimpleSchema.oneOf(Array,Object,String),
+        optional: true
+    },
+    'profile.$': {
+        type: String
+    }
+  });
 
 //TODO: collection options seperate
 const collectionOptions =
@@ -12,7 +34,29 @@ const collectionOptions =
         authRequired : true,
         roleRequired : "admin"
     },
-    excludedEndpoints: ['patch']
+    excludedEndpoints: ['patch'],
+    endpoints:
+    {
+        post:
+        {
+            action: function(){
+                const qParams = this.queryParams;
+        	    const bParams = this.bodyParams;
+                let params = {};
+                if(qParams !== undefined) params = {...qParams}
+                if(bParams !== undefined) params = {...params, ...bParams}
+                //checkPost(params);
+                try{
+                    createUserSchema.validate(params);
+                    var tmp = Accounts.createUser({username:params.username,email:params.email,password:params.password,profile:params.profile});
+                    return {status: 'success', data: {userid: tmp}};
+                } catch(error) {
+                  return {statusCode: 500, body: {status: 'fail', message: error.message}};
+                }
+                
+            }
+        }
+    }
 
   };
   Api.addCollection(Meteor.users,collectionOptions);
