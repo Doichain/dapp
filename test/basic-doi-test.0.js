5416bad4dda8628a946bf5c4de49945bb17b2c0e
Nico Krause <nico@le-space.de>
2018-10-29 15:04:05 +0400

    enabled basic tests

--- test/basic-doi-test.0.js
+++ test/basic-doi-test.0.js
@@ -5,36 +5,16 @@ import {
     isNodeAlive,
     isNodeAliveAndConnectedToHost
 } from "./test-api/test-api-on-node";
-import {
-    fetchConfirmLinkFromPop3Mail,
-    getNameIdOfOptIn,
-    login,
-    confirmLink,
-    requestDOI,
-    verifyDOI,
-    createUser,
-    findUser,
-    findOptIn,
-    exportOptIns
-} from "./test-api/test-api-on-dapp";
-import {logBlockchain} from "../imports/startup/server/log-configuration";
 
 const node_url_alice = 'http://172.20.0.6:18332/';
 const node_url_bob =   'http://172.20.0.7:18332/';
 const rpcAuth = "admin:generated-password";
 const privKeyBob = "cP3EigkzsWuyKEmxk8cC6qXYb4ZjwUo5vzvZpAPmDQ83RCgXQruj";
 
-const dappUrlAlice = "http://localhost:3000";
-const dappUrlBob = "http://172.20.0.8:4000";
-const dAppLogin = {"username":"admin","password":"password"};
-const aliceALogin = {"username":"alice-a","password":"password"};
-
 const log = true;
-const templateUrlA="http://templateUrlB.com";
-const templateUrlB="http://templateUrlB.com";
-let aliceAddress;
 
-describe('basic-doi-test', function () {
+
+describe('basic-doi-test-setup', function () {
     this.timeout(300000);
 
     it('should create a RegTest Doichain with alice and bob and some Doi - coins', function (done) {
@@ -43,94 +23,11 @@ describe('basic-doi-test', function () {
         isNodeAliveAndConnectedToHost(node_url_bob,rpcAuth,'alice',false);
         importPrivKey(node_url_bob,rpcAuth,privKeyBob,true,false);
 
-        aliceAddress = getNewAddress(node_url_alice,rpcAuth,false);
-        generatetoaddress(node_url_alice,rpcAuth, aliceAddress,110);  //110 blocks to new address! 110 blöcke *25 coins
+        global.aliceAddress = getNewAddress(node_url_alice,rpcAuth,false);
+        generatetoaddress(node_url_alice,rpcAuth, global.aliceAddress,110);  //110 blocks to new address! 110 blöcke *25 coins
 
         const aliceBalance = getBalance(node_url_alice,rpcAuth,log);
         chai.assert.isAbove(aliceBalance, 0, 'no funding! ');
         done();
     });
-
-    it('should test if basic Doichain workflow is working with data', function (done) {
-          const recipient_mail = "bob@ci-doichain.org"; //please use this as standard to not confuse people!
-          const sender_mail  = "alice@ci-doichain.org";
-        requestConfirmVerifyBasicDoi(recipient_mail,sender_mail,{'city':'Ekaterinburg'},"bob@ci-doichain.org","bob",done);
-    });
-
-    it('should test if basic Doichain workflow is working without optional data', function (done) {
-          const recipient_mail = "alice@ci-doichain.org"; //please use this as an alernative when above standard is not possible
-          const sender_mail  = "bob@ci-doichain.org";
-        requestConfirmVerifyBasicDoi(recipient_mail,sender_mail,null,"alice@ci-doichain.org","alice",done);
-    });
-
-  /*
-    it('should test if basic Doichain workflow is working with co-sponsoring', function (done) {
-        const recipient_mail = "bob@ci-doichain.org"; //please use this as standard to not confuse people!
-        const sender_mail  = "alice-main-sponsor@ci-doichain.org";
-        requestDOI(recipient_mail,sender_mail,{'city':'Ekaterinburg'},"bob@ci-doichain.org","bob",done);
-    });
-    */
-
-    it('should test if Doichain workflow is using different templates for different users', function (done) {
-        const recipient_mail = "bob@ci-doichain.org"; //
-        const sender_mail_alice_a  = "alice-a@ci-doichain.org";
-
-        const logAdmin = login(dappUrlAlice,dAppLogin,false);
-
-        let userA = createUser(dappUrlAlice,logAdmin,"alice-a",templateUrlA,true);
-        chai.expect(findUser(userA)).to.not.be.undefined;
-        let userB = createUser(dappUrlAlice,logAdmin,"alice-b",templateUrlB,true);
-        chai.expect(findUser(userB)).to.not.be.undefined;
-
-        const logUserA = login(dappUrlAlice,aliceALogin,true);
-        const resultDataOptIn = requestDOI(dappUrlAlice,logUserA,recipient_mail,sender_mail_alice_a,null,true);
-        chai.expect(findOptIn(resultDataOptIn.data.id,true)).to.not.be.undefined;
-        done();
-    });
-
-    it('should test if users can export OptIns ', function (done) {
-        const logAdmin = login(dappUrlAlice,dAppLogin,true);
-        const logUserA = login(dappUrlAlice,aliceALogin,true);
-        const exportedOptIns = exportOptIns(dappUrlAlice,logAdmin,true);
-        chai.expect(exportedOptIns).to.not.be.undefined;
-        chai.expect(exportedOptIns[0]).to.not.be.undefined;
-        const exportedOptInsA = exportOptIns(dappUrlAlice,logUserA,true);
-        for(let optIn in exportedOptInsA){
-            chai.expect(optIn.ownerId).to.be.equal(logUserA.userId);
-        }
-        //chai.expect(findOptIn(resultDataOptIn._id)).to.not.be.undefined;
-        done();
-    });
-
-    function requestConfirmVerifyBasicDoi(recipient_mail,sender_mail,optionalData,recipient_pop3username, recipient_pop3password,done){
-        //login to dApp & request DOI on alice via bob
-        const dataLoginAlice = login(dappUrlAlice,dAppLogin,false); //log into dApp
-        const resultDataOptIn = requestDOI(dappUrlAlice,dataLoginAlice,recipient_mail,sender_mail,optionalData,false);
-        generatetoaddress(node_url_alice,rpcAuth, aliceAddress,1,false); //TODO this should be not necessary(!) but with out we have an error when fetching the transaction
-
-        if(log) logBlockchain('waiting seconds before get NameIdOfOptIn',10);
-        setTimeout(Meteor.bindEnvironment(function () {
-
-            const nameId = getNameIdOfOptIn(node_url_alice,rpcAuth,resultDataOptIn.data.id,true);
-
-            if(log) logBlockchain('waiting seconds before fetching email:',10);
-            setTimeout(Meteor.bindEnvironment(function () {
-
-                const link2Confirm= fetchConfirmLinkFromPop3Mail("mail",110,recipient_pop3username,recipient_pop3password,dappUrlBob,false);
-                confirmLink(link2Confirm);
-                generatetoaddress(node_url_alice,rpcAuth, aliceAddress,1,false);
-
-                if(log) logBlockchain('waiting 10 seconds to update blockchain before generating another block:');
-                setTimeout(Meteor.bindEnvironment(function () {
-                    generatetoaddress(node_url_alice,rpcAuth, aliceAddress,1,false);
-
-                    if(log) logBlockchain('waiting 10 seconds before verifying DOI on alice:');
-                    setTimeout(Meteor.bindEnvironment(function () {
-                        verifyDOI(dappUrlAlice, sender_mail, recipient_mail,nameId, dataLoginAlice, log ); //need to generate two blocks to make block visible on alice
-                        done();
-                    }),10000); //verify
-                }),10000); //verify
-            }),15000); //connect to pop3
-        }),10000); //find transaction on bob's node - even the block is not confirmed yet
-    }
 });
--- test/basic-doi-test.1.js
+++ test/basic-doi-test.1.js
@@ -1,20 +1,18 @@
-import {Meteor} from "meteor/meteor";
-chai.use(require('chai-datetime'));
-chai.use(require('chai-date-string'));
 import {chai} from 'meteor/practicalmeteor:chai';
+import {generatetoaddress} from "./test-api/test-api-on-node";
 import {
-    confirmLink,
     fetchConfirmLinkFromPop3Mail,
     getNameIdOfOptIn,
     login,
-    requestDOI, verifyDOI
+    confirmLink,
+    requestDOI,
+    verifyDOI,
+    createUser,
+    findUser,
+    findOptIn,
+    exportOptIns
 } from "./test-api/test-api-on-dapp";
-import {logBlockchain, logError} from "../imports/startup/server/log-configuration";
-import {generatetoaddress, getNewAddress} from "./test-api/test-api-on-node";
-
-const execFile = require('child_process').execFile;
-const exec = require('child_process').exec;
-
+import {logBlockchain} from "../imports/startup/server/log-configuration";
 
 const node_url_alice = 'http://172.20.0.6:18332/';
 const rpcAuth = "admin:generated-password";
@@ -22,146 +20,94 @@ const dappUrlAlice = "http://localhost:3000";
 const dappUrlBob = "http://172.20.0.8:4000";
 const dAppLogin = {"username":"admin","password":"password"};
 const log = true;
-let aliceAddress;
 
-describe('basic-doi-test-with-offline-node', function () {
-    this.timeout(600000);
+const templateUrlA="http://templateUrlB.com";
+const templateUrlB="http://templateUrlB.com";
+const aliceALogin = {"username":"alice-a","password":"password"};
 
-    after(function(){
-        exec('sudo docker stop 3rd_node', (e, stdout, stderr)=> {
-            logBlockchain('stopped 3rd_node:',{stdout:stdout,stderr:stderr});
-            exec('sudo docker rm 3rd_node', (e, stdout2, stderr2)=> {
-                logBlockchain('stopped 3rd_node:',{stdout:stdout2,stderr:stderr2});
-            });
-        });
+describe('basic-doi-test', function () {
+    this.timeout(300000);
+
+    it('should test if basic Doichain workflow is working with optional data', function (done) {
+        const recipient_mail = "bob@ci-doichain.org"; //please use this as standard to not confuse people!
+        const sender_mail  = "alice@ci-doichain.org";
+        requestConfirmVerifyBasicDoi(recipient_mail,sender_mail,{'city':'Ekaterinburg'},"bob@ci-doichain.org","bob",done);
     });
 
-    it('should test if basic Doichain workflow is working when Bobs node is temporarily offline', function(done) {
-        aliceAddress = getNewAddress(node_url_alice,rpcAuth,false);
-        //shutdown Bob
-        start3rdNode();
-        stopDockerBob();
-        const recipient_mail = "bob@ci-doichain.org";
-        const sender_mail  = "alice-to-offline-node@ci-doichain.org";
-        const recipient_pop3username = "bob@ci-doichain.org";
-        const recipient_pop3password = "bob";
+    it('should test if basic Doichain workflow is working without optional data', function (done) {
+        const recipient_mail = "alice@ci-doichain.org"; //please use this as an alernative when above standard is not possible
+        const sender_mail  = "bob@ci-doichain.org";
+        requestConfirmVerifyBasicDoi(recipient_mail,sender_mail,null,"alice@ci-doichain.org","alice",done);
+    });
 
+    /*
+         it('should test if basic Doichain workflow is working with co-sponsoring', function (done) {
+             const recipient_mail = "bob@ci-doichain.org"; //please use this as standard to not confuse people!
+             const sender_mail  = "alice-main-sponsor@ci-doichain.org";
+             requestDOI(recipient_mail,sender_mail,{'city':'Ekaterinburg'},"bob@ci-doichain.org","bob",done);
+         });
+
+
+       it('should test if Doichain workflow is using different templates for different users', function (done) {
+           const recipient_mail = "bob@ci-doichain.org"; //
+           const sender_mail_alice_a  = "alice-a@ci-doichain.org";
+
+           const logAdmin = login(dappUrlAlice,dAppLogin,false);
+
+           let userA = createUser(dappUrlAlice,logAdmin,"alice-a",templateUrlA,true);
+           chai.expect(findUser(userA)).to.not.be.undefined;
+           let userB = createUser(dappUrlAlice,logAdmin,"alice-b",templateUrlB,true);
+           chai.expect(findUser(userB)).to.not.be.undefined;
+
+           const logUserA = login(dappUrlAlice,aliceALogin,true);
+           const resultDataOptIn = requestDOI(dappUrlAlice,logUserA,recipient_mail,sender_mail_alice_a,null,true);
+           chai.expect(findOptIn(resultDataOptIn.data.id,true)).to.not.be.undefined;
+           done();
+       });
+
+       it('should test if users can export OptIns ', function (done) {
+           const logAdmin = login(dappUrlAlice,dAppLogin,true);
+           const logUserA = login(dappUrlAlice,aliceALogin,true);
+           const exportedOptIns = exportOptIns(dappUrlAlice,logAdmin,true);
+           chai.expect(exportedOptIns).to.not.be.undefined;
+           chai.expect(exportedOptIns[0]).to.not.be.undefined;
+           const exportedOptInsA = exportOptIns(dappUrlAlice,logUserA,true);
+           for(let optIn in exportedOptInsA){
+               chai.expect(optIn.ownerId).to.be.equal(logUserA.userId);
+           }
+           //chai.expect(findOptIn(resultDataOptIn._id)).to.not.be.undefined;
+           done();
+       });     */
+
+    function requestConfirmVerifyBasicDoi(recipient_mail,sender_mail,optionalData,recipient_pop3username, recipient_pop3password,done){
         //login to dApp & request DOI on alice via bob
-        let dataLoginAlice = {};
-        let resultDataOptIn = {};
-
-        if(log) logBlockchain('logging in alice and request DOI');
-        dataLoginAlice = login(dappUrlAlice, dAppLogin, false); //log into dApp
-        resultDataOptIn = requestDOI(dappUrlAlice, dataLoginAlice, recipient_mail, sender_mail, null, false);
-
-        generatetoaddress(node_url_alice, rpcAuth, aliceAddress, 1, false); //TODO this should be not necessary(!) but with out we have an error when fetching the transaction
+        const dataLoginAlice = login(dappUrlAlice,dAppLogin,false); //log into dApp
+        const resultDataOptIn = requestDOI(dappUrlAlice,dataLoginAlice,recipient_mail,sender_mail,optionalData,false);
+        generatetoaddress(node_url_alice,rpcAuth, global.aliceAddress,1,false); //TODO this should be not necessary(!) but with out we have an error when fetching the transaction
 
         if(log) logBlockchain('waiting seconds before get NameIdOfOptIn',10);
-        Meteor.setTimeout(function () {
+        setTimeout(Meteor.bindEnvironment(function () {
+
             const nameId = getNameIdOfOptIn(node_url_alice,rpcAuth,resultDataOptIn.data.id,true);
-            startDockerBob();
-            if(log) logBlockchain('waiting seconds before connecting nodes again:');
-            connectDockerBob();
-            //generating a block so transaction gets confirmed and delivered to bob.
-            if(log) logBlockchain('waiting seconds before fetching email:');
-            Meteor.setTimeout(function () {
-                const link2Confirm = fetchConfirmLinkFromPop3Mail("mail", 110, recipient_pop3username, recipient_pop3password, dappUrlBob, false);
+
+            if(log) logBlockchain('waiting seconds before fetching email:',10);
+            setTimeout(Meteor.bindEnvironment(function () {
+
+                const link2Confirm= fetchConfirmLinkFromPop3Mail("mail",110,recipient_pop3username,recipient_pop3password,dappUrlBob,false);
                 confirmLink(link2Confirm);
-                generatetoaddress(node_url_alice, rpcAuth, aliceAddress, 1, false);
-                if (log) logBlockchain('waiting 10 seconds to update blockchain before generating another block:');
-                Meteor.setTimeout(function () {
-                    generatetoaddress(node_url_alice, rpcAuth, aliceAddress, 1, false);
+                generatetoaddress(node_url_alice,rpcAuth, global.aliceAddress,1,false);
 
-                    if (log) logBlockchain('waiting 10 seconds before verifying DOI on alice:');
-                    Meteor.setTimeout(function () {
-                        verifyDOI(dappUrlAlice, sender_mail, recipient_mail, nameId, dataLoginAlice, log); //need to generate two blocks to make block visible on alice
+                if(log) logBlockchain('waiting 10 seconds to update blockchain before generating another block:');
+                setTimeout(Meteor.bindEnvironment(function () {
+                    generatetoaddress(node_url_alice,rpcAuth, global.aliceAddress,1,false);
+
+                    if(log) logBlockchain('waiting 10 seconds before verifying DOI on alice:');
+                    setTimeout(Meteor.bindEnvironment(function () {
+                        verifyDOI(dappUrlAlice, sender_mail, recipient_mail,nameId, dataLoginAlice, log ); //need to generate two blocks to make block visible on alice
                         done();
-                    }, 10000); //verify
-                }, 10000); //generatetoaddress
-            },10000); //connect to pop3
-        },10000); //find transaction on bob
-    }); //it
+                    }),10000); //verify
+                }),10000); //verify
+            }),15000); //connect to pop3
+        }),10000); //find transaction on bob's node - even the block is not confirmed yet
+    }
 });
-
-
-export function stopDockerBob(client) {
-    const syncFunc = Meteor.wrapAsync(stop_docker_bob);
-    return syncFunc(client);
-}
-
-function stop_docker_bob(callback) {
-    execFile('sudo', ['docker','stop','doichain-dapp_bob_1'], (error, stdout, stderr) => {
-        if (error) {
-            logError('docker_stop_bob:',stderr);
-            callback(stderr, stdout);
-        }
-        logBlockchain('stopped bobs node:',stdout);
-        callback(null, stdout);
-    });
-}
-
-export function start3rdNode(client) {
-    const syncFunc = Meteor.wrapAsync(start_3rd_node);
-    return syncFunc(client);
-}
-
-function start_3rd_node(callback) {
-
-    exec('sudo docker run --expose=18332 ' +
-        '-e REGTEST=true ' +
-        '-e DOICHAIN_VER=0.0.6 ' +
-        '-e RPC_ALLOW_IP=::/0 ' +
-        '-e CONNECTION_NODE=alice '+
-        '-e RPC_PASSWORD=generated-password ' +
-        '--name=3rd_node '+
-        '--dns=172.20.0.5  ' +
-        '--dns=8.8.8.8 ' +
-        '--dns-search=ci-doichain.org ' +
-        '--ip=172.20.0.9 ' +
-        '--network=doichain-dapp_static-network -d doichain/core:0.0.6', (e, stdout, stderr)=> {
-        callback(stderr, stdout);
-    });
-}
-
-export function startDockerBob(client) {
-    const syncFunc = Meteor.wrapAsync(start_docker_bob);
-    return syncFunc(client);
-}
-
-function start_docker_bob(callback) {
-    execFile('sudo', ['docker','start','doichain-dapp_bob_1'], (error, stdout, stderr) => {
-        if (error) {
-            logError('docker_start_bob:',stderr);
-            callback(stderr, stdout);
-        }
-        logBlockchain('started bobs node:',stdout);
-        callback(null, stdout);
-    });
-}
-
-
-export function sleepDockerBob(client) {
-    const syncFunc = Meteor.wrapAsync(sleep_docker_bob);
-    return syncFunc(client);
-}
-
-function sleep_docker_bob(callback) {
-    execFile('sleep', ['3'], (error, stdout, stderr) => {
-        if (error) {
-            logError('docker_sleep_bob:',stderr);
-            callback(stderr, stdout);
-        }
-        callback(null, stdout);
-    });
-}
-
-export function connectDockerBob(client) {
-    const syncFunc = Meteor.wrapAsync(connect_docker_bob);
-    return syncFunc(client);
-}
-
-function connect_docker_bob(callback) {
-    exec('sudo docker exec -d doichain-dapp_bob_1 doichaind -regtest -reindex -addnode=alice', (e, stdout, stderr)=> {
-        callback(stderr, stdout);
-    });
-}
