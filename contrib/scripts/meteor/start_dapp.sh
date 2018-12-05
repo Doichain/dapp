3f98ba309e12b349d46018e81bee207b1b576ecd
Nico Krause <nico@le-space.de>
2018-12-05 14:09:32 +0400

    removing build directory with sudo

--- contrib/scripts/meteor/start_dapp.sh
+++ contrib/scripts/meteor/start_dapp.sh
@@ -3,7 +3,7 @@
 #build dApp for bob
 printf "build/\\ntest/" > .meteorignore #don't include tests into build
 meteor npm install --save-exact @babel/runtime@7.0.0-beta.55
-rm -rf build && meteor build build/ --architecture os.linux.x86_64 --directory #this takes a long and could be excluded when not needed.
+sudo rm -rf build && meteor build build/ --architecture os.linux.x86_64 --directory #this takes a long and could be excluded when not needed.
 cd build/bundle/programs/server && npm install
 cd - #go back to project root
 
@@ -19,5 +19,5 @@ export MONGO_URL=mongodb://mongo:27017/bob
 nohup node build/bundle/main.js &
 
 #overwrite .meteorignore back to normal
-sudo echo "build/" > .meteorignore
+#sudo echo "build/" > .meteorignore
 
