6b1200a3924b6c565670368747d2cd8a8c37bdea
Nico Krause <nico@le-space.de>
2019-01-07 19:33:44 +0400

    changed npm packages

--- packages/doichain-meteor-api/client/main.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import '/imports/startup/client';
-import { renderRoutes } from '../imports/startup/client/routes.js';
-
-Meteor.startup(() => {
-  render(renderRoutes(), document.getElementById('app'));
-});
