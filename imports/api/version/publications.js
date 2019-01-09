82f05216e2e1785f5b13a52ba9abc47b057fe508
Nico Krause <nico@le-space.de>
2019-01-09 14:11:27 +0400

    version restored

---
+++ imports/api/version/publications.js
@@ -0,0 +1,6 @@
+import { Meteor} from 'meteor/meteor';
+import { Meta } from '../meta/meta';
+
+Meteor.publish('version', function version() {
+    return Meta.find({});
+});
--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -2,6 +2,6 @@ import '../../api/languages/methods.js';
 import '../../api/doichain/methods.js';
 import '../../api/recipients/server/publications.js';
 import '../../api/opt-ins/methods.js';
-//import '../../api/version/publications.js';
+import '../../api/version/publications.js';
 import '../../api/opt-ins/server/publications.js';
 
