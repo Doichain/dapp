189468fc46566e96e26393693fe5b5961f03232a
Nico Krause <nico@le-space.de>
2019-02-04 11:00:21 +0800

    added meta and sender subscription, improved opt-ins-ui

--- client/components/OptIns/index.js
+++ client/components/OptIns/index.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react"
 import MUIDataTable from "mui-datatables";
-import {OptInsCollection,RecipientsCollection} from "meteor/doichain:doichain-meteor-api";
+import {OptInsCollection,RecipientsCollection, SendersCollection, MetaCollection} from "meteor/doichain:doichain-meteor-api";
 import {withTracker} from "meteor/react-meteor-data";
+import _ from 'lodash';
 import {Meteor} from "meteor/meteor";
 
 class OptInsPage  extends Component {
@@ -13,23 +14,32 @@ class OptInsPage  extends Component {
     render () {
         const {
             theme,
-            optIns
+            meta,
+            optIns,
+            recipients,
+            senders
         } = this.props
 
         //const columns = ["_id","createdAt","nameId","ownerId","recipient","sender","error"];
-        const columns = ["recipient","sender","createdAt","error"];
+        const columns = [
+            "sender",
+            "recipient",
+            "createdAt",
+            "error"];
         let data = [];
+
+        console.log(meta);
 
         optIns.map(doc => {
             const _id = doc._id;
             const createdAt = doc.createdAt.toISOString();
             //const nameId = doc.nameId ? doc.nameId : "";
             //const ownerId = doc.ownerId ? doc.ownerId : "";
-            const recipientId = doc.recipient ? doc.recipient : "";
-            const sender = doc.sender ? doc.sender : "";
+            const sender = doc.sender && senders.length>0 ?  _.find(senders, { _id: doc.sender}).email  : "";
+            const recipientId = doc.recipient ? _.find(recipients, { _id: doc.recipient}).email: "";
             const error = doc.error ? replaceAll(doc.error,"\"", "") : "";
             //const newRecord = [_id, createdAt, nameId, ownerId, recipientId, sender, error];
-            const newRecord = [recipientId, sender, createdAt, error];
+            const newRecord = [sender, recipientId, createdAt, error];
             data.push(newRecord);
         });
 
@@ -51,17 +61,29 @@ class OptInsPage  extends Component {
         )
     }
 }
+
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp(find, 'g'), replace);
 }
+
 export default withTracker(() => {
+
+    const metaHandle = Meteor.subscribe('meta.all');
     const optInsHandle = Meteor.subscribe('opt-ins.all');
+    const sendersHandle = Meteor.subscribe('senders.byOwner');
     const recipientsHandle = Meteor.subscribe('recipients.byOwner');
+
+    console.log('meta ready:'+metaHandle.ready());
+    console.log('senders ready:'+sendersHandle.ready());
+    console.log('optIns ready:'+optInsHandle.ready());
+    console.log('recipientsHandle ready:'+recipientsHandle.ready());
     const loading = !optInsHandle.ready()||!recipientsHandle.ready();
 
     return {
         loading,
+        meta: metaHandle.ready() ? MetaCollection.find().fetch() : [],
         optIns: !loading ? OptInsCollection.find().fetch() : [],
-        recipients: recipientsHandle.ready() ? RecipientsCollection.find().fetch() : []
+        recipients: recipientsHandle.ready() ? RecipientsCollection.find().fetch() : [],
+        senders: sendersHandle.ready() ? SendersCollection.find().fetch() : []
     }
 })(OptInsPage)
