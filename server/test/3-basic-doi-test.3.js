8becc3fde9c38ad05a825995ebd2a719d0228944
Nico Krause <nico@le-space.de>
2019-03-01 18:50:15 +0800

    minor improvements

--- server/test/3-basic-doi-test.3.js
+++ server/test/3-basic-doi-test.3.js
@@ -9,37 +9,34 @@ import {
 } from "meteor/doichain:doichain-meteor-api";
 import {deleteOptInsFromAliceAndBob, generatetoaddress, getNewAddress} from "./test-api/test-api-on-node";
 
-const node_url_alice = 'http://172.20.0.6:18332/';
-const rpcAuthAlice = "admin:generated-password";
-const dappUrlAlice = "http://localhost:3000";
-const dappUrlBob = "http://172.20.0.8:4000";
-const dAppLogin = {"username":"admin","password":"password"};
 const recipient_pop3username = "bob@ci-doichain.org";
 const recipient_pop3password = "bob";
 
 if(Meteor.isAppTest) {
     xdescribe('03-basic-doi-test-03', function () {
-
+        this.timeout(0);
+        
         before(function () {
             logBlockchain("removing OptIns,Recipients,Senders");
-            deleteOptInsFromAliceAndBob();
-            deleteAllEmailsFromPop3("mail", 110, recipient_pop3username, recipient_pop3password, true);
+            //deleteOptInsFromAliceAndBob();
+            //deleteAllEmailsFromPop3(global.inside_docker?"mail":"localhost", 110, recipient_pop3username, recipient_pop3password, true);
         });
 
         it('should test if basic Doichain workflow running 5 times', function (done) {
             this.timeout(0);
 
-            const dataLoginAlice = login(dappUrlAlice, dAppLogin, false); //log into dApp
-            global.aliceAddress = getNewAddress(node_url_alice, rpcAuthAlice, false);
-            for (let i = 0; i < 20; i++) {
+            const dataLoginAlice = login(global.dappUrlAlice, global.dAppLogin, false); //log into dApp
+            global.aliceAddress = getNewAddress(global.node_url_alice, global.rpcAuthAlice, false);
+            for (let i = 0; i < 5; i++) {
                 const recipient_mail = "bob@ci-doichain.org"; //please use this as standard to not confuse people!
                 const sender_mail = "alice_" + i + "@ci-doichain.org";
-                requestConfirmVerifyBasicDoi(node_url_alice, rpcAuthAlice, dappUrlAlice, dataLoginAlice, dappUrlBob, recipient_mail, sender_mail, {'city': 'Ekaterinburg_' + i}, "bob@ci-doichain.org", "bob", true);
+                requestConfirmVerifyBasicDoi(global.node_url_alice, global.rpcAuthAlice,global.dappUrlAlice,
+                    dataLoginAlice,global.dappUrlBob,recipient_mail, sender_mail, {'city': 'Ekaterinburg_' + i}, "bob@ci-doichain.org", "bob", true);
             }
             done();
         });
 
-        it('should test if basic Doichain workflow running 20 times without confirmation and verification', function (done) {
+        xit('should test if basic Doichain workflow running 20 times without confirmation and verification', function (done) {
             this.timeout(0);
             deleteAllEmailsFromPop3("mail", 110, recipient_pop3username, recipient_pop3password, true);
             const dataLoginAlice = login(dappUrlAlice, dAppLogin, false); //log into dApp
@@ -53,7 +50,7 @@ if(Meteor.isAppTest) {
             done();
         });
 
-        it('should test if basic Doichain workflow running 100 times with without confirmation and verification', function (done) {
+        xit('should test if basic Doichain workflow running 100 times with without confirmation and verification', function (done) {
             this.timeout(0);
             deleteAllEmailsFromPop3("mail", 110, recipient_pop3username, recipient_pop3password, true);
             const dataLoginAlice = login(dappUrlAlice, dAppLogin, false); //log into dApp
